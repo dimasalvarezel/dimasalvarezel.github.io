947ba3a1f6ff1e408d4a088e68ae931a8d73a805
dimasalvarezel <dimasalvarezel@gmail.com>
2020-08-24 14:32:56 +0700

    project pertama

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout/main')
+
+@section('title', 'Dimas alvarezel susendi')
+
+@section('container')
+ <div class="container">
+  <div class="row">
+    <div class="col-10">
+    
+    <h1 class="mt-2">Index Dimas ALvarezel!</h1>
+   
+    </div>
+  </div>
+ </div>
+ @endsection
+
+   
